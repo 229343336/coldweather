46d37a84895961f714e5e20c84cac1fc48283e0d
Cold.H <229343336@qq.com>
2015-03-18 14:23:15 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/com/coldweather/app/db/ColdWeatherOpenHelper.java
@@ -0,0 +1,13 @@
+package com.coldweather.app.db;
+
+public class ColdWeatherOpenHelper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/coldweather/app/model/ColdWeatherDB.java
@@ -0,0 +1,13 @@
+package com.coldweather.app.model;
+
+public class ColdWeatherDB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
